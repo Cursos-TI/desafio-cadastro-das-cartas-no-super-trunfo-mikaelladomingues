2621e73fc86eaf6fe9de4db788a5228cf55f1059
Mikaella Araujo Domingues <mikaelladomingues05@gmail.com>
2025-08-07 21:18:17 +0000

    novo comando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@
 //Teste larissa
 
 int main() {
-    //carta 1, Pará.
+    //carta 1
     char estado1[50];
     char código_carta1[4];
     char cidade1[50];
@@ -15,6 +15,8 @@ int main() {
     float área_km01;
     float PIB1;
     int turísticos1;
+    float resultado1;
+    float resultado2;
 
     printf("Digite seu Estado: ");
     scanf("%s", estado1);
@@ -44,9 +46,15 @@ int main() {
     scanf("%d", &turísticos1); 
     printf("Número de pontos Turísticos: %d\n", turísticos1);
 
+    resultado1 = (população1 / área_km01);
+    printf("Densidade Populacional: %f\n", resultado1);
+   
+    resultado2 = (PIB1 / população1);
+    printf("PIB per Capita: %f\n", resultado2);
+
     printf("\n"); //adicionei para separar as informações.
 
-    //carta 2, BAHIA.
+    //carta 2
     char estado2[50];
     char código_carta2[4];
     char cidade2[50];
@@ -54,6 +62,8 @@ int main() {
     float área_km02;
     float PIB2;
     int turísticos2;
+    float resultado3;
+    float resultado4;
 
     printf("Digite seu Estado: ");
     scanf("%s", estado2);
@@ -82,6 +92,12 @@ int main() {
     printf("Digite seu Número de pontos Turísticos: ");
     scanf("%d", &turísticos2);
     printf("Números de pontos turísticos: %d\n", turísticos2);
+
+    resultado3 = (população2 / área_km02);
+    printf("Densidade Populacional: %f\n", resultado3);
+
+    resultado4 = (PIB2 / população2);
+    printf("PIB per Capita: %f\n", resultado4);
 
     return 0;
 }
